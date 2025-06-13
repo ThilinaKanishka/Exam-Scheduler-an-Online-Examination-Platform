38bbd342ff24983318dadca354893e485a4397d3
ThilinaKanishka <thilina20010313@gmail.com>
2025-06-14 00:23:48 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Registration from "./components/Registration";
 import Logging from "./components/Logging";
-
 import Home from "./components/Home";
 import Adminpanel from "./components/Adminpanel";
 import AdminScheduleExa from "./components/AdminShedualExam";
